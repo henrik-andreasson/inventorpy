ac54e30399346263595030edd474062a07e7e495
Henrik Andreasson <henrik.andreasson@cgi.com>
2020-07-26 21:28:06 +0200

    cleanup main script

--- inventorpy.py
+++ inventorpy.py
@@ -1,7 +1,5 @@
 from app import create_app, db, cli
 from app.models import User, Service
-from app.modules.server.models import Server
-from app.modules.hsm.models import HsmDomain, HsmPed, HsmPin, HsmPciCard, HsmBackupUnit
 
 app = create_app()
 cli.register(app)
@@ -9,4 +7,4 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Service': Service}
