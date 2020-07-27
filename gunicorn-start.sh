813c5a7d03808c3ab5fa073a574d62d32db77e99
Henrik Andreasson <henrik.andreasson@cgi.com>
2020-07-27 22:15:09 +0200

    change dir to install_path to support both os-install and docker in gunicon-start

--- gunicorn-start.sh
+++ gunicorn-start.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+if [ "x${INSTALL_PATH}" == "x" ] ; then
+
+	if [ -d "/inventorpy" ] ; then
+		INSTALL_PATH=/inventorpy
+	elif [ -d "/opt/inventorpy" ] ; then
+		INSTALL_PATH=/opt/inventorpy
+	fi
+fi
+
+cd "${INSTALL_PATH}"
+
 USE_CERT=0
 
 if [ "x$CERT" != "x" ] ; then
-  echo "$CERT" | tr ';' '\n' > /inventorpy/cert.pem
+  echo "$CERT" | tr ';' '\n' > "${INSTALL_PATH}/cert.pem"
   let USE_CERT="$USE_CERT + 1"
 fi
 
 if [ "x$CA" != "x" ] ; then
-  echo "$CA" | tr ';' '\n' > /inventorpy/ca.pem
+  echo "$CA" | tr ';' '\n' > "${INSTALL_PATH}/ca.pem"
   let USE_CERT="$USE_CERT + 1"
 fi
 
 if [ "x$KEY" != "x" ] ; then
-  echo "$KEY" | tr ';' '\n' > /inventorpy/key.pem
+  echo "$KEY" | tr ';' '\n' > "${INSTALL_PATH}/key.pem"
   let USE_CERT="$USE_CERT + 1"
 fi
 
@@ -35,9 +46,9 @@ fi
 if [ $USE_CERT -gt 1 ] ; then
 
     gunicorn inventorpy:app -b 0.0.0.0:${LISTEN} \
-         --pid /inventorpy/teamplan.pid \
-         --keyfile /inventorpy/key.pem  \
-         --certfile  /inventorpy/cert.pem ${EXTRA_OPTIONS}
+         --pid "${INSTALL_PATH}/teamplan.pid" \
+         --keyfile "${INSTALL_PATH}/key.pem"  \
+         --certfile  "${INSTALL_PATH}/cert.pem" ${EXTRA_OPTIONS}
 
 else
 
